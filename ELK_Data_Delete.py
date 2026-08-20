d8db72eaf458c52647ade463a24823656038158e
yagnikcode <120770264+yagnikcode@users.noreply.github.com>
2026-08-20 13:18:05 +0530

    Implement ELK data deletion script
    This script connects to an Oracle database and Elasticsearch, fetching distinct values from a specified table and deleting corresponding documents from Elasticsearch based on the provided query field.

---
+++ ELK_Data_Delete.py
@@ -0,0 +1,171 @@
+import cx_Oracle
+# import oracledb
+import pandas as pd
+from elasticsearch import Elasticsearch, helpers
+from datetime import datetime
+import pytz
+import sys
+from dotenv import load_dotenv
+import importlib.util
+import os
+import requests
+import json
+import multiprocessing
+
+# Adjust number of workers based on CPU count
+num_workers = min(8, multiprocessing.cpu_count() * 2)
+
+load_dotenv()
+
+# # Create a connection pool
+# pool = oracledb.create_pool(
+#     user=os.getenv('olap_prod_user_name'),
+#     password=os.getenv('olap_prod_password'),
+#     dsn=f"{os.getenv('olap_prod_host_address')}:{os.getenv('olap_prod_port')}/{os.getenv('olap_prod_service_name')}"
+# )
+
+# Set up the connection pool
+pool = cx_Oracle.SessionPool(
+    user=os.getenv('olap_prod_user_name'),
+    password=os.getenv('olap_prod_password'),
+    dsn=f"{os.getenv('olap_prod_host_address')}:{os.getenv('olap_prod_port')}/{os.getenv('olap_prod_service_name')}"
+)
+
+def fetch_value_list(query):
+    with pool.acquire() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchall()
+            # Convert the result to a list
+            return [row[0] for row in result]
+
+def dynamic_import(module_name):
+    current_dir = os.path.dirname(__file__)  # Get current directory
+    module_path = os.path.join(current_dir, f"{module_name}.py")  # Construct file path
+
+    # Load the module dynamically
+    spec = importlib.util.spec_from_file_location(module_name, module_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    return module  # Return the imported module
+
+
+def create_es_connection():
+    """Elastic Search Connection Establish"""
+    print("CONNECTING TO ES.............")
+    es = Elasticsearch(hosts=os.getenv('elk_host_address'), http_auth=(os.getenv('elk_user_name'),os.getenv('elk_password')), timeout=800, max_retries=10, retry_on_timeout=True)
+    return es
+
+def fetch_db_data(query,chunksize=100000):
+    """Fetch data from DB in chunks"""
+    print("FETCHING DATA..............")
+    with pool.acquire() as connection:
+        for chunk in pd.read_sql_query(query, connection, chunksize=chunksize):
+            yield chunk
+
+def bulk_insert_to_es(es, actions):
+    """Bulk inserts data to Elasticsearch"""
+    helpers.bulk(es, actions)
+
+# Function to chunk the list
+def chunk_list(data, chunk_size):
+    for i in range(0, len(data), chunk_size):
+        yield data[i:i + chunk_size]
+
+# Function to count documents using Elasticsearch API
+def count_documents(elk_host,elk_port,elk_index, field_name, values_chunk):
+    url = f"http://{elk_host}:{elk_port}/{elk_index}/_count"
+    query = {
+        "query": {
+            "terms": {
+                f"{field_name.upper()}.keyword": values_chunk
+            }
+        }
+    }
+    response = requests.post(url, json=query)
+    if response.status_code == 200:
+        return response.json().get("count", 0)
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return 0
+
+def delete_data(elk_host,elk_port,elk_index,query_field,query_field_value_list, timeout="2m"):
+    print("DELETING DATA FROM ES.............")
+    # ES URL
+    es_url = f"http://{elk_host}:{elk_port}/{elk_index}/_delete_by_query?timeout={timeout}"
+
+    # Define the search query
+    delete_query = {
+        "query": {
+            "bool": {
+                "must": [
+                {
+                    "terms": {
+                    f"{query_field.upper()}.keyword": query_field_value_list
+                    }
+                }
+                ]
+            }
+        }
+    }
+    print(delete_query)
+    # Send the request to Elasticsearch
+    response = requests.post(es_url,headers={"Content-Type": "application/json"},data=json.dumps(delete_query))
+    print(response)
+    # Check the response status
+    if response.status_code == 200:
+        results = response.json()
+        print("Deleted documents:", results.get("deleted", 0))
+    else:
+        print("Error:", response.status_code, response.text)
+
+def main(index_name,query_field):
+    # Connection to Oracle DB and Elasticsearch
+    start_time = datetime.now(pytz.timezone('Asia/Kolkata'))
+    print("Start time:", start_time.strftime('%Y-%m-%d %H:%M:%S'))
+
+    module = dynamic_import(index_name)
+    table_name = 'customer_del_rec'
+
+    es = create_es_connection()
+    if not es.indices.exists(index=index_name): # check index exist or not
+        module.index_creation_in_elastic_search(es,index_name) # index creation in es
+        print(f"Index {index_name} created in ES")
+
+    query_value_list = f'select distinct {query_field} from {table_name}'
+    query_field_value_list = fetch_value_list(query_value_list)
+
+    print(query_value_list)
+    print(f'Total {query_field} : ',len(query_field_value_list))
+
+    total_elk_count = 0
+    # Process the values in chunks
+    for chunk in chunk_list(query_field_value_list, chunk_size=10000):
+        count = count_documents(os.getenv('elk_host'),os.getenv('elk_port'), index_name, query_field, chunk)
+        total_elk_count += count
+
+    print(f"{index_name} - For {len(query_field_value_list)} {query_field}s Total document count in ELK : {total_elk_count}")
+
+    batch_size = 10000
+    count_rotation = 1
+    for i in range(0, len(query_field_value_list), batch_size):
+        print("Round - ",count_rotation)
+        count_rotation += 1
+        batch_list = query_field_value_list[i:i + batch_size]
+        print('Batch size : ',len(batch_list))
+        print('Batch list : ',batch_list[0],'.....',batch_list[-1])
+        delete_data(os.getenv('elk_host'),os.getenv('elk_port'),index_name,query_field,batch_list)
+
+    es.indices.refresh(index=index_name)
+
+    print("time:", datetime.now(pytz.timezone('Asia/Kolkata')).strftime('%Y-%m-%d %H:%M:%S'))
+    end_time = datetime.now(pytz.timezone('Asia/Kolkata'))
+    print("End time:", end_time.strftime('%Y-%m-%d %H:%M:%S'))
+    print(f"Time taken: {(end_time - start_time).total_seconds():.2f} seconds")
+    print("INDEXING COMPLETED")
+
+if __name__ == '__main__':
+    index_name = sys.argv[1]
+    query_field = sys.argv[2]
+    main(index_name,query_field)
